ab5d3fc377f4bf236a8343d89e3ac3b70c12f586
Admin <Admin@DESKTOP-JJ9G0NT>
2022-01-30 20:28:25 +0700

    commit module nguoi dung

--- PRJ_SEARCH/PRJ_SEARCH/Content/JsCustom/NguoiDung.js
+++ PRJ_SEARCH/PRJ_SEARCH/Content/JsCustom/NguoiDung.js
@@ -1,4 +1,179 @@
-﻿//Hàm gọi form đăng nhập
-function ShowModalAddEdit() {
-    $('#mdlNguoiDung_AddEdit').modal('show');
+﻿function resetForm() {
+    $('#errtaikhoan').hide();
+    $('#errmatkhau').hide();
+    $('#errten').hide();
+    $('#id').val("0");
+    $('#varTaiKhoan').val("");
+    $('#varMatKhau').val("");
+    $('#varHoTen').val("");
+    $('#varEmail').val("");
+    $('#varPhone').val("");
+    $('#varDiaChi').val("");
+    $('#varGhiChu').val("");
+}
+
+function ShowModalAddEdit(id) {
+    resetForm();
+    if (id == null || id == 0) {
+        document.getElementById('TieuDe').innerHTML = "Thêm mới người dùng";
+    }
+    else {
+        document.getElementById('TieuDe').innerHTML = "Cập nhật thông tin người dùng";
+        $.ajax({
+            url: "/NguoiDung/GetData",
+            data: {
+                id: id
+            },
+            type: 'post',
+            success: function (result) {
+                if (result.status == true) {
+                    $('#id').val(id);
+                    $('#varTaiKhoan').val(result.data.TenDangNhap);
+                    $('#varMatKhau').val(result.data.MatKhau);
+                    $('#varHoTen').val(result.data.HoTen);
+                    $('#varEmail').val(result.data.Email);
+                    $('#varPhone').val(result.dataSoDienThoai);
+                    $('#varDiaChi').val(result.data.DiaChi);
+                    $('#varGhiChu').val(result.data.DoPhuCTap);
+                }
+            }
+        });
+    }
+    $("#mdlNguoiDung_AddEdit").modal("show");
+}
+
+function Save() {
+    var isSave = true;
+    var hoten = $('#varHoTen').val().trim();
+    var taikhoan = $('#varTaiKhoan').val().trim();
+    var matkhau = $('#varMatKhau').val().trim();
+    var id = $('#id').val();
+    if (hoten == "") {
+        isSave = false;
+        document.getElementById('errten').innerHTML = "Không được để trống.";
+        $('#errten').show();
+    }
+    if (taikhoan == "") {
+        isSave = false;
+        document.getElementById('errtaikhoan').innerHTML = "Không được để trống.";
+        $('#errtaikhoan').show();
+    }
+    if (matkhau == "") {
+        isSave = false;
+        document.getElementById('errmatkhau').innerHTML = "Không được để trống.";
+        $('#errmatkhau').show();
+    }
+    if (isSave == true) {
+        var formData = new FormData();
+        formData.append("HoTen", hoten);
+        formData.append("TenDangNhap", taikhoan);
+        formData.append("MatKhau", matkhau);
+        formData.append("Phone", $('#varPhone').val());
+        formData.append("DiaChi", $('#varDiaChi').val());
+        formData.append("Email", $('#varEmail').val());
+        formData.append("ID", id);
+        formData.append("DoPhucTap", $('#varGhiChu').val());
+
+        $.ajax({
+            async: false,
+            type: 'POST',
+            url: "/NguoiDung/Save",
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function (response) {
+                if (response.status == true) {
+                    $('#mdlNguoiDung_AddEdit').modal('hide');
+                    if (id == 0) {
+                        alert('Thêm mới người dùng thành công.')
+                    }
+                    else {
+                        alert('Cập nhật người dùng thành công.')
+                    }
+                    loadPartial();
+                   
+                }
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    }
+}
+
+var idND = "";
+function Link_DeleteTT_onclick(DID) {
+    idND = DID;
+    if (ListCTLID != "")//view popup xác nhận xóa
+    {
+        $("#DeleteND").modal("show");
+    }
+}
+
+function deleteOne() {
+    $.ajax({
+        url: '/NguoiDung/DeleteAll',
+        dataType: 'json',
+        type: 'post',
+        data: { lstIdString: idND },
+        success: function (rs) {
+            //location.reload();
+            hideLoadingScreen();
+            if (rs.status == true) {
+                var mangIdDaXoa = JSON.parse(rs.data);
+                for (var i = 0; i < mangIdDaXoa.length; i++) {
+                    $('#tblNguoiDung > tbody > tr[data-id="' + mangIdDaXoa[i] + '"]').remove();
+                }
+                var message = 'Xóa thành công người dùng.';
+                alert(message);
+                loadPartial();
+
+            }
+        }
+    })
+}
+
+function loadPartial() {
+    var trangHienTai = 1;
+    var strKeyWord = $('#txtSearch').val('');
+    mangId = [];
+    $.ajax({
+        url: '/NguoiDung/GetListData?page=' + trangHienTai,
+        data:
+        {
+            keyword: strKeyWord.val(),
+        },
+        type: 'GET',
+        success: function (res) {
+            $('#tblNguoiDung').html(res);
+            //$('#tblNguoiDung').find('tbody  tr[data-id]').each(function () {
+            //    idOfIiem = $(this).attr('data-id');
+            //    if ($.inArray(Number(idOfIiem), mangId) >= 0) {
+            //        $(this).find('.one-delete-js').prop('checked', true);
+            //    }
+            //})
+        },
+        error: function () {
+           
+        }
+    });
+}
+
+function Search() {
+    //Lấy thông tin tại các ô tìm kiếm
+    var ten = $('#txtSearch').val();
+    //Gọi hàm ajax
+    $.ajax({
+        url: '/NguoiDung/GetListData',
+        data:
+        {
+            keyword: ten,
+        },
+        success: function (res) {
+            $('#tblNguoiDung').html(res);
+            //Lấy thông tin tại các ô tìm kiếm
+            $('#txtSearch').val(ten);
+        }
+    })
 }
--- PRJ_SEARCH/PRJ_SEARCH/Controllers/NguoiDungController.cs
+++ PRJ_SEARCH/PRJ_SEARCH/Controllers/NguoiDungController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -26,7 +29,8 @@ namespace PRJ_SEARCH
                 Email = k.Email,
                 HoTen = k.HoTen,
                 TenDangNhap = k.TenDangNhap,
-                SoDienThoai = k.SoDienThoai
+                SoDienThoai = k.SoDienThoai,
+                ID= k.ID
             }).ToList();
 
             totalRecord = lstData.Count();
@@ -43,6 +47,104 @@ namespace PRJ_SEARCH
                 PagingInfo = pagingInfo
             };
             return PartialView(model);
+        }
+
+        [HttpPost]
+        public JsonResult GetData(int id)
+        {
+            tb_NguoiDung item = db.tb_NguoiDungs.FirstOrDefault(x => x.ID == id);
+            return Json(new
+            {
+                status = true,
+                data = item
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [ValidateInput(false)]
+        public JsonResult Save(FormCollection c)
+        {
+            int ID = int.Parse(c["ID"].ToString());
+            //int userId = int.Parse(Session["MaNguoiDung"].ToString());
+            if (ID == 0)
+            {
+                //thêm mới
+                tb_NguoiDung item = new tb_NguoiDung();
+                item.HoTen = c["HoTen"] != null ? c["HoTen"].Trim() : "";
+                item.TenDangNhap = c["TenDangNhap"] != null ? c["TenDangNhap"].Trim() : "";
+                item.MatKhau = c["MatKhau"];
+                item.SoDienThoai = c["Phone"] != null ? c["Phone"].Trim() : "";
+                item.DiaChi = c["DiaChi"] != null ? c["DiaChi"].Trim() : "";
+                item.Email = c["Email"] != null ? c["Email"].Trim() : "";
+                item.DoPhucTap = c["DoPhucTap"] != null ? c["DoPhucTap"].Trim() : "";
+                item.NgayTao = DateTime.Now;
+                //tam thoi chua lam nut dang nhap nguoi dung nen de tam nguoi tao la 1
+                item.NguoiTao = 1;
+                item.TrangThai = true;
+                db.tb_NguoiDungs.InsertOnSubmit(item);
+                db.SubmitChanges();
+            }
+            else
+            {
+                //sửa
+                tb_NguoiDung data = db.tb_NguoiDungs.FirstOrDefault(x => x.ID == ID);
+                data.HoTen = c["HoTen"] != null ? c["HoTen"].Trim() : "";
+                data.TenDangNhap = c["TenDangNhap"] != null ? c["TenDangNhap"].Trim() : "";
+                data.MatKhau = c["MatKhau"];
+                data.SoDienThoai = c["Phone"] != null ? c["Phone"].Trim() : "";
+                data.DiaChi = c["DiaChi"] != null ? c["DiaChi"].Trim() : "";
+                data.Email = c["Email"] != null ? c["Email"].Trim() : "";
+                data.DoPhucTap = c["DoPhucTap"] != null ? c["DoPhucTap"].Trim() : "";
+                data.NgaySua = DateTime.Now;
+                data.NguoiSua = 1;
+                db.SubmitChanges();
+            }
+            return Json(new
+            {
+                status = true
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public int Delete(string ListID)
+        {
+            //int userId = int.Parse(Session["MaNguoiDung"].ToString());
+            var convertArray = ListID.Replace("[", "").Replace("]", "");
+            var lstLoai = convertArray.Split(',').ToList();
+            foreach (var item in lstLoai)
+            {
+                int id = int.Parse(item);
+                tb_NguoiDung objDel = db.tb_NguoiDungs.FirstOrDefault(k => k.ID == id);
+                if (objDel != null)
+                {
+                    objDel.TrangThai = false;
+                }
+                db.SubmitChanges();
+            }
+            return 1;
+
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> DeleteAll(string lstIdString)
+        {
+            if (lstIdString == null || lstIdString.Length < 0)
+                return Json(new
+                {
+                    status = false
+                }, JsonRequestBehavior.AllowGet);
+
+            int CountCreateDDAP = Delete(lstIdString);
+            if (CountCreateDDAP <= 0)
+            {
+                return Json(new
+                {
+                    status = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new
+            {
+                status = true,
+                data = lstIdString,
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
