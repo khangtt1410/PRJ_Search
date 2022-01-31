d99ac0b09654acc64a3e7d113bb401a259e7a7fe
khangtt <khangtt@huongvietgroup.com>
2022-01-31 13:20:37 +0700

    commit update - KhangTT

--- PRJ_SEARCH/PRJ_SEARCH/Content/JsCustom/DangNhap.js
+++ PRJ_SEARCH/PRJ_SEARCH/Content/JsCustom/DangNhap.js
@@ -1,1 +1,46 @@
-﻿
+﻿//Hàm xử lý đăng nhập
+function DangNhap() {
+    var taiKhoan = $('#txtTenDangNhap').val();
+    var matKhau = $('#txtMatKhau').val();
+    $.ajax({
+        url: '/CheckValidAccount/DangNhap',
+        type: 'POST',
+        dataType: 'JSON',
+        data: {
+            taikhoan: taiKhoan,
+            matkhau: matKhau
+        },
+        success: function (res) {
+            if (res.status == true) {
+                location.reload();
+            } else {
+                $('#errDangNhap').show();
+                $('#errDangNhap span').html(res.mess);
+            }
+        },
+        error: function (res) {
+
+        }
+    })
+}
+//Hàm xử lý đăng xuất
+function DangXuat() {
+    $.ajax({
+        url: '/CheckValidAccount/DangXuat',
+        type: 'POST',
+        dataType: 'JSON',
+        success: function (res) {
+            if (res.status == true) {
+                location.reload();
+            } 
+        },
+        error: function (res) {
+
+        }
+    })
+}
+//Hàm xử lý sự kiện keyup của textbox tài khoản và mật khẩu
+function ResetError() {
+    $('#errDangNhap').hide();
+    $('#errDangNhap span').html('');
+}
---
+++ PRJ_SEARCH/PRJ_SEARCH/Controllers/CheckValidAccountController.cs
@@ -0,0 +1,71 @@
+﻿using CLSHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PRJ_SEARCH.Controllers
+{
+    public class CheckValidAccountController : Controller
+    {
+        SearchDBDataContext db = new SearchDBDataContext();
+        // GET: DangNhap
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Hàm xử lý đăng nhập
+        /// </summary>
+        /// <param name="taikhoan"></param>
+        /// <param name="matkhau"></param>
+        /// <returns></returns>
+        public JsonResult DangNhap(string taikhoan, string matkhau)
+        {
+            bool status = true;
+            string mess = "";
+
+            //Lấy tài khoản của người dùng có thông tin tài khoản, mật khẩu tương ứng
+            tb_NguoiDung nguoiDung = db.tb_NguoiDungs.FirstOrDefault(k => k.TenDangNhap == taikhoan && k.TrangThai == true);
+            if(nguoiDung != null)
+            {
+                matkhau = LMSUtilities.GetMD5(string.Concat(nguoiDung.DoPhucTap, matkhau));
+                if(matkhau == nguoiDung.MatKhau)
+                {
+                    status = true;
+                    //Lưu mã người dùng vào session - bộ nhớ tạm
+                    Session["IDNguoiDung"] = nguoiDung.ID;
+                }
+                else
+                {
+                    status = false;
+                    mess = "Mật khẩu không chính xác";
+                }
+            }
+            else
+            {
+                status = false;
+                mess = "Tài khoản không chính xác";
+            }
+            return Json(new
+            {
+                status = status,
+                mess = mess
+            }, JsonRequestBehavior.AllowGet);
+        }
+        /// <summary>
+        /// Hàm xử lý đăng xuất
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult DangXuat()
+        {
+            //Xóa dữ liệu session đã lưu
+            Session.Clear();
+            return Json(new
+            {
+                status = true
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
