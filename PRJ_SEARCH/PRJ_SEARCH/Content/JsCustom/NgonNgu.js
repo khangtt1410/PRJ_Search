6cce023d624d8963f0ef13dd970fcdf3a4bcdc36
Admin <Admin@DESKTOP-JJ9G0NT>
2022-02-02 22:54:04 +0700

    commit module ngon ngu

--- PRJ_SEARCH/PRJ_SEARCH/Content/JsCustom/NgonNgu.js
+++ PRJ_SEARCH/PRJ_SEARCH/Content/JsCustom/NgonNgu.js
@@ -1,1 +1,170 @@
-﻿
+﻿function resetForm() {
+    $('#errorNameCourse').hide();
+    $('#id').val("0");
+    $('#txtten').val("");
+}
+
+$("#txtten").keyup(function () {
+    var ten = $('#txtten').val().trim();
+    if (ten != "") {
+        $('#errorNameCourse').hide();
+    }
+    else {
+        document.getElementById('errorNameCourse').innerHTML = "Không được để trống.";
+        $('#errorNameCourse').show();
+    }
+})
+
+function ShowModalAddEdit(id) {
+    resetForm();
+    if (id == null || id == 0) {
+        document.getElementById('TieuDe').innerHTML = "Thêm mới ngôn ngữ";
+    }
+    else {
+        document.getElementById('TieuDe').innerHTML = "Cập nhật thông tin ngôn ngữ";
+        $.ajax({
+            url: "/NgonNgu/GetData",
+            data: {
+                id: id
+            },
+            type: 'post',
+            success: function (result) {
+                if (result.status == true) {
+                    $('#id').val(id);
+                    $('#txtten').val(result.data.TenNgonNgu);
+                }
+            }
+        });
+    }
+    $("#mdlNgonNgu_AddEdit").modal("show");
+}
+
+function Search() {
+    //Lấy thông tin tại các ô tìm kiếm
+    var ten = $('#txtSearch').val();
+    //Gọi hàm ajax
+    $.ajax({
+        url: '/NgonNgu/GetListData',
+        data:
+        {
+            keyword: ten,
+        },
+        success: function (res) {
+            $('#tblNgonNgu').html(res);
+            //Lấy thông tin tại các ô tìm kiếm
+            $('#txtSearch').val(ten);
+        }
+    })
+}
+
+//hàm xử lý khi nhấn chuyển trang 
+$(document).on('click', '#tblNgonNgu .paginationNavigator li a', function (e) {
+    e.preventDefault();
+    if ($(this).attr('href') == undefined || $.trim($(this).attr('href')) == "") return;
+    //Lấy thông tin tại các ô tìm kiếm
+    var ten = $('#txtSearch').val();
+    $.ajax({
+        url: $(this).attr('href'),
+        data:
+        {
+            keyword: ten
+        },
+        type: 'GET',
+        success: function (res) {
+            $('#tblNgonNgu').html(res);
+
+            $('#DataTables_Table_0_paginate ul li.currentPage').addClass('currentpage');
+            //Lấy thông tin tại các ô tìm kiếm
+            $('#txtSearch').val(ten);
+        }
+    });
+});
+
+function loadPartial() {
+    var trangHienTai = 1;
+    var strKeyWord = $('#txtSearch').val('');
+    mangId = [];
+    $.ajax({
+        url: '/NgonNgu/GetListData?page=' + trangHienTai,
+        data:
+        {
+            keyword: strKeyWord.val(),
+        },
+        type: 'GET',
+        success: function (res) {
+            $('#tblNgonNgu').html(res);
+        },
+        error: function () {
+
+        }
+    });
+}
+
+function Save() {
+    var isSave = true;
+    var ten = $('#txtten').val().trim();
+    var id = $('#id').val();
+    if (ten == "") {
+        isSave = false;
+        document.getElementById('errorNameCourse').innerHTML = "Không được để trống.";
+        $('#errorNameCourse').show();
+    }
+    if (isSave == true) {
+        var formData = new FormData();
+        formData.append("TenNgonNgu", ten);
+        formData.append("ID", id);
+
+        $.ajax({
+            async: false,
+            type: 'POST',
+            url: "/NgonNgu/Save",
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function (response) {
+                if (response.status == true) {
+                    $('#mdlNgonNgu_AddEdit').modal('hide');
+                    if (id == 0) {
+                        $('#noidung').text('Thêm mới ngôn ngữ thành công.')
+
+                    }
+                    else {
+                        $('#noidung').text('Cập nhật ngôn ngữ thành công.')
+                    }
+                    $('#modalThongBao').modal('show');
+                    loadPartial();
+                }
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    }
+}
+
+var idNN = "";
+function Link_DeleteTT_onclick(ID) {
+    idNN = ID;
+    if (idNN != "")//view popup xác nhận xóa
+    {
+        $("#Delete").modal("show");
+    }
+}
+
+function deleteOne() {
+    $.ajax({
+        url: '/NgonNgu/Delete',
+        dataType: 'json',
+        type: 'post',
+        data: { ID: idNN },
+        success: function (rs) {
+            if (rs.status == true) {
+                $("#Delete").modal("hide");
+                $('#noidung').text('Xóa ngôn ngữ thành công.')
+                $('#modalThongBao').modal('show');
+                loadPartial();
+            }
+        }
+    })
+}
--- PRJ_SEARCH/PRJ_SEARCH/Controllers/NgonNguController.cs
+++ PRJ_SEARCH/PRJ_SEARCH/Controllers/NgonNguController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +9,106 @@ namespace PRJ_SEARCH.Controllers
 {
     public class NgonNguController : Controller
     {
+        SearchDBDataContext db = new SearchDBDataContext();
         // GET: NgonNgu
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetListData(string keyword = "", int page = 1, int pageSize = 6)
+        {
+            int totalRecord = 0;
+            List<NgonNguEntities.NgonNguView> lstData = new List<NgonNguEntities.NgonNguView>();
+            lstData = db.tb_NgonNgus.Where(k => k.TrangThai == true
+            && (k.TenNgonNgu.Contains(keyword) || keyword == "")).OrderByDescending(k => k.ID)
+            .Select(k => new NgonNguEntities.NgonNguView
+            {
+                TenNgonNgu = k.TenNgonNgu,
+                TrangThai = k.TrangThai,
+                ID = k.ID
+            }).ToList();
+
+            totalRecord = lstData.Count();
+            lstData = lstData.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            var pagingInfo = new CLSHelper.Models.PagingInfo();
+            pagingInfo.CurrentPage = page;
+            pagingInfo.ItemsPerPage = pageSize;
+            pagingInfo.TotalItems = totalRecord;
+
+            NgonNguEntities.lstData model = new NgonNguEntities.lstData()
+            {
+                lstDoc = lstData,
+                PagingInfo = pagingInfo
+            };
+            return PartialView(model);
+        }
+
+        [HttpPost]
+        public JsonResult GetData(int id)
+        {
+            tb_NgonNgu item = db.tb_NgonNgus.FirstOrDefault(x => x.ID == id);
+            return Json(new
+            {
+                status = true,
+                data = item
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [ValidateInput(false)]
+        public JsonResult Save(FormCollection c)
+        {
+            int userId = 1;
+            int ID = int.Parse(c["ID"].ToString());
+            //int userId = int.Parse(Session["MaNguoiDung"].ToString());
+            if (ID == 0)
+            {
+                //thêm mới
+                tb_NgonNgu item = new tb_NgonNgu();
+                item.TenNgonNgu = c["TenNgonNgu"] != null ? c["TenNgonNgu"].Trim() : "";
+                item.NgayTao = DateTime.Now;
+                item.NguoiTao = userId;
+                item.TrangThai = true;
+                db.tb_NgonNgus.InsertOnSubmit(item);
+                db.SubmitChanges();
+            }
+            else
+            {
+                //sửa
+                tb_NgonNgu data = db.tb_NgonNgus.FirstOrDefault(x => x.ID == ID);
+                data.TenNgonNgu = c["TenNgonNgu"] != null ? c["TenNgonNgu"].Trim() : "";
+                data.NgaySua = DateTime.Now;
+                data.NguoiSua = userId;
+                db.SubmitChanges();
+            }
+            return Json(new
+            {
+                status = true
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Delete(int ID)
+        {
+            if (ID == null || ID == 0)
+            {
+                return Json(new
+                {
+                    status = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+            tb_NgonNgu objDel = db.tb_NgonNgus.FirstOrDefault(k => k.ID == ID);
+            if (objDel != null)
+            {
+                objDel.TrangThai = false;
+            }
+            db.SubmitChanges();
+
+            return Json(new
+            {
+                status = true
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
